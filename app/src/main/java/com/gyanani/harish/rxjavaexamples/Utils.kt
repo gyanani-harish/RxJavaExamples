c504018ca31a671f6b1651e4fdb76f94d79f7240
Harish Gyanani <gyanani.harish@gmail.com>
2021-03-27 16:10:50 +0530

    added async task and coroutine for comparison

---
+++ app/src/main/java/com/gyanani/harish/rxjavaexamples/Utils.kt
@@ -0,0 +1,42 @@
+package com.gyanani.harish.rxjavaexamples
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+object Utils {
+    fun apiCalling(): String {
+        val url = URL("https://jsonplaceholder.typicode.com/todos/1")
+        val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+        urlConnection.requestMethod = "GET"
+
+        val inputStream: InputStream = urlConnection.inputStream
+        return readStream(inputStream)
+    }
+
+    fun readStream(inputStream: InputStream): String {
+        var reader: BufferedReader? = null
+        val response = StringBuffer()
+        try {
+            reader = BufferedReader(InputStreamReader(inputStream))
+            var line: String? = ""
+            while (reader.readLine().also { line = it } != null) {
+                response.append(line)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        return response.toString()
+    }
+}
